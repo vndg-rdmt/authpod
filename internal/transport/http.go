be7dd19b4c57ec98672a15e9eb5a3819304da45f
Даниил Белоусов <daniilbelousov@MacBook-Pro-Daniil.local>
2024-10-27 23:22:58 +0300

    done

--- internal/transport/http.go
+++ internal/transport/http.go
@@ -2,11 +2,19 @@ package transport
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/vndg-rdmt/authpod/internal/controller"
 )
 
 func NewHttp(ctr *controller.Fiber, host string) error {
 	app := fiber.New()
+	app.Use(cors.New(cors.Config{
+		AllowOriginsFunc: func(_ string) bool {
+			return true
+		},
+		AllowHeaders:     "",
+		AllowCredentials: true,
+	}))
 	app.Post("/api/sign-in", ctr.SignIn)
 	app.Get("/api/ping", ctr.Ping)
 	app.Put("/api/tokens/:user_id", ctr.IssueToken)
